dfc6c7f7e60c929ed345f98dcb8ecc29ed97c42d
camilod529 <camilod529@gmail.com>
2023-10-31 14:46:12 -0500

    Buscando por nombre

--- champions/urls.py
+++ champions/urls.py
@@ -3,9 +3,15 @@ from rest_framework import routers
 from champions import views
 
 router = routers.DefaultRouter()
-router.register(r"champions", views.ChampionViewSet)
-router.register(r"champions/<int:pk>", views.ChampionViewSet)
+router.register(r"champions", views.ChampionViewSet, basename="champions")
+router.register(r"champions/<int:pk>", views.ChampionViewSet, basename="champions-by-id")
+
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "champions/name/<str:name>/",
+        views.ChampionViewSet.as_view(actions={"get": "list"}),
+        name="champion-detail-by-name",
+    ),
 ]
--- champions/views.py
+++ champions/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
+from django.shortcuts import get_object_or_404, render
+from rest_framework import viewsets, generics, permissions
+from rest_framework.views import Response
 from champions.models import Champion
 from champions.serializers import ChampionSerializer
 
 
 # Create your views here.
-class ChampionViewSet(viewsets.ModelViewSet):
-    queryset = Champion.objects.all().order_by("name")
+class ChampionViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ChampionSerializer
+    queryset = Champion.objects.all()
+
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        if name:
+            try:
+                champ = Champion.objects.filter(name__iexact=name)
+                return champ
+            except Champion.DoesNotExist:
+                return Response({"detail", f"Campeón con nombre {name}no encontrado"}, status=404)
+        return super().get_queryset()
